2b2330bc33643ad0da93cd96627ce8ae60ac325c
lzzy2747 <151424090+lzzy2747@users.noreply.github.com>
2024-06-14 11:34:05 +0900

    chore: change value to int

--- src/main/java/com/example/upsdowns2/UpsDownsApplication.java
+++ src/main/java/com/example/upsdowns2/UpsDownsApplication.java
@@ -11,7 +11,7 @@ public class UpsDownsApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(UpsDownsApplication.class.getResource("upsdowns-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 685.0, 478.0);
+        Scene scene = new Scene(fxmlLoader.load(), 685, 478);
         stage.setTitle("Ups&Downs Library");
         stage.setResizable(false);
         stage.setScene(scene);
